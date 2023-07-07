62f0c9367cfac0b7ee003f7d9f155cfa4c68a66f
Kirat Mokha <kiratmokha@Kirats-MacBook-Pro.local>
2023-07-06 19:28:17 -0500

    Trying new tests...

--- Project4/test_attackChoice.py
+++ Project4/test_attackChoice.py
@@ -1,10 +1,7 @@
 import battle_sim
-import mock
+from pytest import MonkeyPatch
 
 def test_attackChoice1():
-    with mock.patch.object(__builtins__, 'input', lambda: '1'):
-        assert battle_sim.attackChoice() == 1
-        
-def test_attackChoice2():
-    with mock.patch.object(__builtins__, 'input', lambda: '1'):
-        assert battle_sim.attackChoice() == 2
+    MonkeyPatch.setattr("builtins.input", lambda _:"1")
+    i = battle_sim.attackChoice()
+    assert int(i) == 1
--- Project4/test_initiative.py
+++
@@ -1,7 +0,0 @@
-import battle_sim
-import random
-
-def test_initiative():
-    random.seed(1)
-    
-    assert(battle_sim.initiative() == 2)
